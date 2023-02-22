f3c0a36602547e11533f20a3405710db6c41ec4f
Nik <53766@novasbe.pt>
2023-02-22 13:25:46 +0000

    Changed the plot from matplot to seaborn and formatted the code to fit pylint standards

--- method5.py
+++ method5.py
@@ -3,45 +3,36 @@ My Part for the project of group 8
 """
 
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
-ag_df = pd.read_csv("file.csv")
-
+df = pd.read_csv(".nik/file.csv")
 
 def method5(countries):
     """
     This method receives a list of countries or a single country as input and creates a plot of the
-    total output quantity over time for the selected countries. The plot includes a line for each 
-    country and the x-axis is the year.
+    total output quantity over time for the selected countries. The plot includes a line for 
+    each country and the x-axis is the year.
     """
     try:
         if isinstance(countries, list):
-            df_countries = ag_df[ag_df["Entity"].isin(countries)]
-            total_output = (
-                df_countries.groupby(["Entity", "Year"])["output_quantity"]
-                .sum()
-                .reset_index()
-            )
-            for country in countries:
-                country_df = total_output[total_output["Entity"] == country]
-                plt.plot(
-                    country_df["Year"], country_df["output_quantity"], label=country
-                )
-            plt.xlabel("Year")
-            plt.ylabel("Output Quantity")
-            plt.legend()
+            df_countries = df[df['Entity'].isin(countries)]
+            total_output = df_countries.groupby(['Entity', 'Year'])['output_quantity']\
+                .sum().reset_index()
+            #Create the plot for each country
+            plt.figure(figsize=(10, 6))
+            ax_output = sns.lineplot(x='Year', y='output_quantity', hue='Entity', data=total_output)
+            ax_output.set(xlabel='Year', ylabel='Output Quantity')
+            ax_output.legend(loc='upper left', bbox_to_anchor=(1, 1))
             plt.show()
         elif isinstance(countries, str):
-            df_country = ag_df[ag_df["Entity"] == countries]
-            total_output = (
-                df_country.groupby("Year")["output_quantity"].sum().reset_index()
-            )
-            plt.plot(
-                total_output["Year"], total_output["output_quantity"], label=countries
-            )
-            plt.xlabel("Year")
-            plt.ylabel("Output Quantity")
-            plt.legend()
+            df_country = df[df['Entity'] == countries]
+            total_output = df_country.groupby('Year')['output_quantity'].sum().reset_index()
+            #Create the plot for each country
+            plt.figure(figsize=(10, 6))
+            ax_output = sns.lineplot(x='Year', y='output_quantity', data=total_output)
+            ax_output.set(xlabel='Year', ylabel='Output Quantity')
+            ax_output.legend([countries], loc='upper left', bbox_to_anchor=(1, 1))
             plt.show()
         else:
             raise ValueError("Input should be a string or a list of strings")
@@ -56,5 +47,4 @@ def method5(countries):
     finally:
         print("Execution complete\n")
 
-
-method5(["Germany", "France"])
+method5(["Germany", "France", "United States"])
